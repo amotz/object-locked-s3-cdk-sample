d0c6d00f5a0b079d91d92958c138e7600a84c615
amotz <mochida.atsushi@classmethod.jp>
2021-06-08 07:40:48 +0900

    build: add object locked s3 bucket

--- lib/object-locked-s3-cdk-sample-stack.ts
+++ lib/object-locked-s3-cdk-sample-stack.ts
@@ -1,9 +1,31 @@
 import * as cdk from '@aws-cdk/core';
-
+import * as s3 from "@aws-cdk/aws-s3";
 export class ObjectLockedS3CdkSampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = "sample-object-locked-bucket-99999999999"
+    new s3.CfnBucket(this, "sampleObjectLockedBucket", {
+      bucketName: bucketName,
+      bucketEncryption: {
+        serverSideEncryptionConfiguration: [
+          {
+            serverSideEncryptionByDefault: {
+              sseAlgorithm: "AES256"
+            }
+          }
+        ]
+      },
+      objectLockEnabled: true,
+      objectLockConfiguration: {
+        objectLockEnabled: "Enabled",
+        rule: {
+          defaultRetention: {
+            mode: "GOVERNANCE",
+            days: 1
+          }
+        }
+      }
+    });
   }
 }
